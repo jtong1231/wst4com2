0756813b370c816cf9cbaf289ce8685c097ed27d
jtong1231 <jtong5995@gmail.com>
2025-12-29 18:31:09 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["cd.ganajnb2ucn7.top"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["cd5.ganajnb2ucn7.top"];
 pub const RS_PUB_KEY: &str = "gRP1LZ3P4dMEatpfCA4qfNVTETTCmmn7cPkK+uX3HVk=";
 pub const RENDEZVOUS_PORT: i32 = 47106;
 pub const RELAY_PORT: i32 = 47107;
