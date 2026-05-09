adc3c4965278aade528ff5b1e4169946c61c7847
jtong1231 <jtong5995@gmail.com>
2026-05-09 20:58:33 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["cd2.fuognewukjdfhuf15.top"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["cd4.fuognewukjdfhuf15.top"];
 pub const RS_PUB_KEY: &str = "gRP1LZ3P4dMEatpfCA4qfNVTETTCmmn7cPkK+uX3HVk=";
 pub const RENDEZVOUS_PORT: i32 = 50196;
 pub const RELAY_PORT: i32 = 50197;
