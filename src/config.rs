ca7048ee3a60c3784820bdd0863389a4599bd1fe
jtong1231 <jtong5995@gmail.com>
2026-08-02 19:25:08 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -99,12 +99,12 @@ const CHARS: &[char] = &[
     '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
-pub const RENDEZVOUS_SERVERS: &[&str] = &["cd4.feih5whbfh12.top"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["cd4.feig43agfdhtrh13.top"];
 pub const RS_PUB_KEY: &str = "gRP1LZ3P4dMEatpfCA4qfNVTETTCmmn7cPkK+uX3HVk=";
-pub const RENDEZVOUS_PORT: i32 = 58796;
-pub const RELAY_PORT: i32 = 58797;
-pub const WS_RENDEZVOUS_PORT: i32 =58798;
-pub const WS_RELAY_PORT: i32 = 58799;
+pub const RENDEZVOUS_PORT: i32 = 58896;
+pub const RELAY_PORT: i32 = 58897;
+pub const WS_RENDEZVOUS_PORT: i32 =58898;
+pub const WS_RELAY_PORT: i32 = 58899;
 
 macro_rules! serde_field_string {
     ($default_func:ident, $de_func:ident, $default_expr:expr) => {
